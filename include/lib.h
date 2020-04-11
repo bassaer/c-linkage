80a0ddad60559690452988d967dba8a60434c0e4
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-04-11 21:40:06 +0900

    fix

--- include/lib.h
+++ include/lib.h
@@ -1,6 +1,7 @@
 #ifndef LIB_H
 #define LIB_H
 
+void init(int count, char* message);
 void work();
 
 #endif
--- lib.c
+++ lib.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <lib.h>
 
-static int private_counter = 0;
-int public_counter = 0;
+static int private_counter;
+static char *private_message;
+
+int public_counter;
+char *public_message;
+
+void init(int count, char *message) {
+  private_counter = count;
+  private_message = message;
+  public_counter = count;
+  public_message = message;
+}
 
 void work() {
   printf("private_count = %d\n", private_counter);
+  printf("private_message = %s\n", private_message);
   printf("public_count = %d\n", public_counter);
+  printf("public_message = %s\n", public_message);
 }
